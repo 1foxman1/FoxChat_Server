d164d9abb119e67b270fcf1ed095178227b651ac
Foxman <ron.mikhlin7@gmail.com>
2015-02-17 19:07:38 +0200

    list crap

--- FoxChat_Server.py
+++ FoxChat_Server.py
@@ -8,29 +8,44 @@ chatList = []
 
 def main():
   TCP_IP = socket.gethostbyname(socket.gethostname())
-  TCP_PORT = 1740
+  TCP_PORT = 1997
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.bind( (TCP_IP, TCP_PORT ) )
   sock.listen(3)
-  
+
+  print "Server online and awaiting connections!"
+
   while True:
     conn, addr = sock.accept()
     print "Connection address: ", addr
     try:
-      thread.start_new_thread( talk, (conn, data) )
+      thread.start_new_thread( recieve, (conn, data) )
+      thread.start_new_thread( send, (conn, data, chatList, chatList) )
     except:
       print "Error: unable to start thread"
 
 
-def talk(conn, data):
-  while 1
-    data = conn.recv(BUFFER_SIZE)
-    chatList = pickle.loads(data)
-    data = pickle.dumps(data)
-    conn.send(data)
 
-  conn.close()
+
+def recieve(conn, data):
+  print "listening"
+  while True:
+    print "ey"
+    dt = conn.recv(BUFFER_SIZE)
+    print dt
+    chatList.append(dt)
+    
+
+def send(conn, data, curChatList, chatList):
+    while 1:
+        if curChatList != chatList:
+            print chatList
+            data = pickle.dumps(chatList)
+            conn.send(data)
+            chatList = curChatList
+            
+    
 
 if __name__ == "__main__":
   main()
